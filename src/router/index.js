2bd07356b8e2ad523eee4f002d5d14712493b665
britneykline <87348224+britneykline@users.noreply.github.com>
2021-12-14 22:00:44 -0500

    added CovidDashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CovidDashboard from '../views/CovidDashboard.vue'
 import Signup from '../views/Signup.vue'
 import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
@@ -12,8 +12,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'CovidDashboard',
+    component: CovidDashboard
   },
   {
     path: '/signup',
---
+++ src/views/CovidDashboard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="display: contents"> <!-- my wrapper div is rendered invisible -->
+    <h1> Daily Covid-19 Statistics </h1>
+    <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map" width="75%" height="600px" border="1px solid black"></iframe>
+    <iframe src="https://ourworldindata.org/grapher/total-deaths-covid-19?country=USA" width="75%" height="600px" border="1px solid black"></iframe>
+    <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map&region=NorthAmerica" width="75%" height="600px" border="1px solid black"></iframe>
+    
+  </div>
+</template>
